ad18052ac0cbd45c690285b4abd208e0bbca8f7d
Matheus Lenke <matheuslenke@gmail.com>
2024-11-19 12:08:06 -0300

    chore: add Diagram

--- src/main/java/org/ontouml/model/Project.java
+++ src/main/java/org/ontouml/model/Project.java
@@ -1,8 +1,13 @@
 package org.ontouml.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+import org.ontouml.model.utils.ProjectMetaProperties;
+import org.ontouml.model.view.View;
 
 /**
  * A named element that serves as the container of an entire OntoUML ontology, including the
@@ -12,12 +17,45 @@ import lombok.experimental.SuperBuilder;
 @EqualsAndHashCode(callSuper = true)
 @Data
 @SuperBuilder
+@NoArgsConstructor
 public class Project extends NamedElement {
   /** Contains the OntoUML elements that are part of the project. */
-  List<String> elements;
+  ProjectMetaProperties metaProperties = new ProjectMetaProperties();
+
+  private Package root;
+
+  private Map<String, Class> classes = new HashMap<>();
+  private Map<String, Package> packages = new HashMap<>();
+  private Map<String, Relation> relations = new HashMap<>();
+  private Map<String, Property> properties = new HashMap<>();
+  private Map<String, Literal> literals = new HashMap<>();
+  private Map<String, GeneralizationSet> generalizationSets = new HashMap<>();
+  private Map<String, Generalization> generalizations = new HashMap<>();
+
+  private List<View> diagrams = new ArrayList<>();
 
   @Override
   public String getType() {
     return "Project";
+  }
+
+  public void setElements(List<ModelElement> elements) {
+    elements.forEach(element -> {
+      if (element instanceof Class) {
+        classes.put(element.getId(), (Class) element);
+      } else if (element instanceof Package) {
+        packages.put(element.getId(), (Package) element);
+      } else if (element instanceof Relation) {
+        relations.put(element.getId(), (Relation) element);
+      } else if (element instanceof Property) {
+        properties.put(element.getId(), (Property) element);
+      } else if (element instanceof Literal) {
+        literals.put(element.getId(), (Literal) element);
+      } else if (element instanceof GeneralizationSet) {
+        generalizationSets.put(element.getId(), (GeneralizationSet) element);
+      } else if (element instanceof Generalization) {
+        generalizations.put(element.getId(), (Generalization) element);
+      }
+    });
   }
 }
---
+++ src/main/java/org/ontouml/model/view/Diagram.java
@@ -0,0 +1,30 @@
+package org.ontouml.model.view;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.SuperBuilder;
+import org.ontouml.model.ModelElement;
+import org.ontouml.model.NamedElement;
+
+import java.util.List;
+
+/**
+ * A named element that contains the visual representation (i.e., the concrete syntax) of an OntoUML
+ * model or of a portion of it.
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@SuperBuilder
+public class Diagram extends NamedElement {
+
+  /** Identifies the model element that is the owner of a diagram. */
+  private ModelElement owner;
+
+  /** Identifies the views contained in the diagram. */
+  private List<View> views;
+
+  @Override
+  public String getType() {
+    return "Diagram";
+  }
+}
