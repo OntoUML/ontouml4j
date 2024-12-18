a627fcd47b521ed5c228d0b97281544434b96e4b
Matheus Lenke <matheuslenke@gmail.com>
2024-12-17 21:13:02 -0300

    feat: GeneralizationView Deserializer
    Signed-off-by: Matheus Lenke <matheuslenke@gmail.com>

--- src/main/java/org/ontouml/model/view/GeneralizationView.java
+++ src/main/java/org/ontouml/model/view/GeneralizationView.java
@@ -1,13 +1,18 @@
 package org.ontouml.model.view;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.ontouml.deserialization.view.GeneralizationViewDeserializer;
 
 /** A view element that represents the single occurrence of a binary relation in a diagram. */
 @EqualsAndHashCode(callSuper = true)
 @Data
 @SuperBuilder
+@NoArgsConstructor
+@JsonDeserialize(using = GeneralizationViewDeserializer.class)
 public class GeneralizationView extends BinaryConnectorView {
   @Override
   public String getType() {
