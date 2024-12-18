4ea0343ba20b06436620d8646192b137aefdf4a1
Matheus Lenke <matheuslenke@gmail.com>
2024-12-18 11:45:19 -0300

    feat: PackageView Deserializer
    Signed-off-by: Matheus Lenke <matheuslenke@gmail.com>

--- src/main/java/org/ontouml/deserialization/view/PackageViewDeserializer.java
+++ src/main/java/org/ontouml/deserialization/view/PackageViewDeserializer.java
@@ -7,18 +7,20 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
 import java.io.IOException;
-import org.ontouml.model.view.ClassView;
+import org.ontouml.model.view.PackageView;
 import org.ontouml.shape.Rectangle;
 
-public class ClassViewDeserializer extends JsonDeserializer<ClassView> {
-
+public class PackageViewDeserializer extends JsonDeserializer<PackageView> {
   @Override
-  public ClassView deserialize(JsonParser parser, DeserializationContext ctxt)
+  public PackageView deserialize(JsonParser parser, DeserializationContext ctxt)
       throws IOException, JacksonException {
     ObjectCodec codec = parser.getCodec();
     JsonNode root = parser.readValueAsTree();
 
-    ClassView view = new ClassView();
+    PackageView view = new PackageView();
+
+    String id = root.get("id").asText();
+    view.setId(id);
 
     String rectangle = root.get("rectangle").asText();
     view.setRectangle(new Rectangle(rectangle));
--- src/main/java/org/ontouml/model/view/PackageView.java
+++ src/main/java/org/ontouml/model/view/PackageView.java
@@ -1,14 +1,20 @@
 package org.ontouml.model.view;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.ontouml.deserialization.view.PackageViewDeserializer;
+import org.ontouml.model.Project;
 import org.ontouml.shape.Rectangle;
 
 /** A view element that represents the single occurrence of a package in a diagram. */
 @EqualsAndHashCode(callSuper = true)
 @Data
 @SuperBuilder
+@NoArgsConstructor
+@JsonDeserialize(using = PackageViewDeserializer.class)
 public class PackageView extends View {
 
   /** Identifies the rectangle shape that renders the package view in the diagram. */
@@ -17,5 +23,16 @@ public class PackageView extends View {
   @Override
   public String getType() {
     return "PackageView";
+  }
+
+  @Override
+  public void resolveAllReferences(Project project) {
+    super.resolveAllReferences(project);
+    if (rectangle == null) {
+      return;
+    }
+    Rectangle rectangle =
+        project.getElementById(getRectangle().getId(), Rectangle.class).orElse(null);
+    this.setRectangle(rectangle);
   }
 }
